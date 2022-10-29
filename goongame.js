4c5685c0a4326f9980847e4c48c380b1bf210ca3
laranc <claranjeira@protonmail.ch>
2022-10-29 17:52:41 +1030

    update go wasm

--- goongame.js
+++ goongame.js
@@ -2,7 +2,4 @@ const goWasm = new Go()
 
 WebAssembly.instantiateStreaming(fetch("goongame.wasm"), goWasm.importObject).then((result) => {
     goWasm.run(result.instance)
-    document.getElementById("print-html").addEventListener("click", () => {
-        document.body.innerHTML += printHtml()
-    })
 })
--- src/main.go
+++ src/main.go
@@ -4,21 +4,71 @@ package main
 
 import (
 	"fmt"
-	"syscall/js"
+	"image/color"
+	"log"
+
+	"github.com/EngoEngine/ecs"
+	"github.com/EngoEngine/engo"
+	"github.com/EngoEngine/engo/common"
 )
 
-var htmlString = "<h1>goon game hahahahaha</h1>"
+type DefaultScene struct{}
 
-func printHtml() js.Func {
-	return js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-		return htmlString
-	})
+type GoonMan struct {
+	ecs.BasicEntity
+	common.RenderComponent
+	common.SpaceComponent
 }
+
+func (*DefaultScene) Preload() {
+	engo.Files.Load("goonman.jpg")
+}
+
+func (*DefaultScene) Setup(u engo.Updater) {
+	w, _ := u.(*ecs.World)
+
+	common.SetBackground(color.White)
+
+	w.AddSystem(&common.RenderSystem{})
+
+	// Retrieve a texture
+	texture, err := common.LoadedSprite("goonman.png")
+	if err != nil {
+		log.Println(err)
+	}
+
+	// Create an entity
+	guy := GoonMan{BasicEntity: ecs.NewBasic()}
+
+	// Initialize the components, set scale to 8x
+	guy.RenderComponent = common.RenderComponent{
+		Drawable: texture,
+		Scale:    engo.Point{8, 8},
+	}
+	guy.SpaceComponent = common.SpaceComponent{
+		Position: engo.Point{0, 0},
+		Width:    texture.Width() * guy.RenderComponent.Scale.X,
+		Height:   texture.Height() * guy.RenderComponent.Scale.Y,
+	}
+
+	// Add it to appropriate systems
+	for _, system := range w.Systems() {
+		switch sys := system.(type) {
+		case *common.RenderSystem:
+			sys.Add(&guy.BasicEntity, &guy.RenderComponent, &guy.SpaceComponent)
+		}
+	}
+}
+
+func (*DefaultScene) Type() string { return "GameWorld" }
 
 func main() {
 	fmt.Printf("goon game\n")
 
-	js.Global().Set("printHtml", printHtml())
-
-	select {}
+	opts := engo.RunOptions{
+		Title:  "GoonGame",
+		Width:  1024,
+		Height: 640,
+	}
+	engo.Run(opts, &DefaultScene{})
 }
