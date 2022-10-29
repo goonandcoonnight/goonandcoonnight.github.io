1d2530e8c1d14062139a47efd97657a4226f9267
laranc <claranjeira@protonmail.ch>
2022-10-29 17:34:18 +1030

    update wasm and js

--- assets/goongame.js
+++ assets/goongame.js
@@ -2,4 +2,7 @@ const goWasm = new Go()
 
 WebAssembly.instantiateStreaming(fetch("goongame.wasm"), goWasm.importObject).then((result) => {
     goWasm.run(result.instance)
+    document.getElementById("print-html").addEventListener("click", () => {
+        document.body.innerHTML += printHtml()
+    })
 })
--- src/main.go
+++ src/main.go
@@ -4,8 +4,19 @@ package main
 
 import (
 	"fmt"
+	"syscall/js"
 )
+
+var htmlString = "<h1>goon game hahahahaha</h1>"
+
+func printHtml() js.Func {
+	return js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		return htmlString
+	})
+}
 
 func main() {
 	fmt.Printf("goon game\n")
+
+	js.Global().Set("printHtml", printHtml())
 }
